671e500d7ff96ac981a28436ab51236730daef7c
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2024-10-10 00:28:14 -0700

    first commit

--- src/app/Contact-us/page.tsx
+++ src/app/Contact-us/page.tsx
@@ -1,19 +1,16 @@
-export default function Contact(){
-    return(
-        <div className="parentContainer">
-            <div className="Form">
-                <div className="text">For contact Fill this Form Thanks</div>
-                <form >
-                    <input className="form-details" type="text" name="name" placeholder="Enter your name"></input>
-                    <br/>
-                    <input className="form-details" type="email" name="email" placeholder="Enter your email"></input>
-                    <br/>
-                    <input className="form-details" type="Comment" name="Comment" placeholder="Write here your quiry"></input>
-                    <br/>
-                    <input className="form-details" type="submit" value="Submit"></input>
-                    <br/>
-                    </form>   
-            </div>
+export default function ContactForm() {
+    return (
+      <div className="parentContainer">
+        <div className="form">
+          <div className="text">For Contact fill this Form Thanks</div>
+          <input className="form-details" type="text" name="name" placeholder="Enter your name" />
+          <br />
+          <input className="form-details" type="email" name="email" placeholder="Enter your email" />
+          <br />
+          <input className="form-details" type="text" name="Comment" placeholder="Write here your query" />
+          <br />
+          <input className="form-details" type="submit" value="Submit" />
         </div>
+      </div>
     );
-}
+  }
